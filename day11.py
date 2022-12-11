abaf1c496f7a48015021ede600404c1a42949ea7
Mario Jaun <mario.jaun@gmail.com>
2022-12-11 15:29:52 +0100

    solve day 11 part 2

--- day11.py
+++ day11.py
@@ -2,14 +2,31 @@ from __future__ import annotations
 
 import re
 import sys
+import math
+
 from typing import Callable, List, Iterable, NamedTuple
 
 
 def main():
-    monkeys = [Monkey.from_input_lines(input_groups) for input_groups in read_input_groups()]
+    input_groups = list(read_input_groups())
+
+    # part one
+    monkeys = [Monkey.from_input_lines(input_group) for input_group in input_groups]
     dispatcher = Dispatcher(monkeys)
 
     for _ in range(20):
+        dispatcher.execute_round()
+
+    print(dispatcher.monkey_business_level())
+
+    # part two
+    monkeys = [Monkey.from_input_lines(input_group) for input_group in input_groups]
+    dispatcher = Dispatcher(monkeys)
+
+    Monkey.worry_levels_divided_by_three = False
+    Monkey.worry_levels_modulo = math.lcm(*[monkey.test_divisor for monkey in monkeys])  # requires Python 3.9
+
+    for i in range(10000):
         dispatcher.execute_round()
 
     print(dispatcher.monkey_business_level())
@@ -33,30 +50,33 @@ class Dispatcher:
 
 
 class Monkey:
+    worry_levels_divided_by_three = True
+    worry_levels_modulo = 0
+
     @staticmethod
     def from_input_lines(input_lines: List[str]):
         assert re.match('^Monkey [0-9]+:$', input_lines[0])
         starting_items = re.match('^Starting items: ([0-9, ]+)$', input_lines[1])
-        operation = re.match('^Operation: new = (\w+) (.) (\w+)$', input_lines[2])
-        test = re.match('^Test: divisible by ([0-9]+)$', input_lines[3])
-        if_true = re.match('^If true: throw to monkey ([0-9]+)$', input_lines[4])
-        if_false = re.match('^If false: throw to monkey ([0-9]+)$', input_lines[5])
+        operation = re.match('^Operation: new = (.*)$', input_lines[2])
+        test_divisor = re.match('^Test: divisible by ([0-9]+)$', input_lines[3])
+        test_true_index = re.match('^If true: throw to monkey ([0-9]+)$', input_lines[4])
+        test_false_index = re.match('^If false: throw to monkey ([0-9]+)$', input_lines[5])
 
         monkey = Monkey()
         monkey.items = [Item(int(value_str)) for value_str in starting_items.group(1).split(', ')]
-        monkey.operation = create_operation(operation.group(1), operation.group(2), operation.group(3))
-        monkey.test = lambda item: item.worry_level % int(test.group(1)) == 0
-        monkey.if_true_index = int(if_true.group(1))
-        monkey.if_false_index = int(if_false.group(1))
+        monkey.operation = create_operation(operation.group(1))
+        monkey.test_divisor = int(test_divisor.group(1))
+        monkey.test_true_index = int(test_true_index.group(1))
+        monkey.test_false_index = int(test_false_index.group(1))
         return monkey
 
     def __init__(self):
         self.items: List[Item] = []
         self.operation: Callable[[Item], None] = lambda item: None
-        self.test: Callable[[Item], bool] = lambda _item: True
-        self.if_true_index: int = 0
-        self.if_false_index: int = 0
-        self.inspect_count: int = 0
+        self.test_divisor = 0
+        self.test_true_index = 0
+        self.test_false_index = 0
+        self.inspect_count = 0
 
     def execute_turn(self) -> Iterable[TurnResult]:
         while self.items:
@@ -64,30 +84,31 @@ class Monkey:
 
             item = self.items.pop(0)
             self.operation(item)
-            item.worry_level = int(item.worry_level / 3)
-            target_index = self.if_true_index if self.test(item) else self.if_false_index
+
+            if Monkey.worry_levels_divided_by_three:
+                item.worry_level = int(item.worry_level / 3)
+            if Monkey.worry_levels_modulo:
+                item.worry_level = item.worry_level % Monkey.worry_levels_modulo
+
+            if value_divisible_by(item.worry_level, self.test_divisor):
+                target_index = self.test_true_index
+            else:
+                target_index = self.test_false_index
+
             yield item, target_index
 
 
-def create_operation(operand1: str, operator: str, operand2: str):
+def create_operation(expression: str):
+    code = compile(expression, '<string>', 'eval')
+
     def operation(item: Item):
-        def parse_operand(operand: str) -> int:
-            if operand == 'old':
-                return item.worry_level
-
-            return int(operand)
-
-        operand1_parsed = parse_operand(operand1)
-        operand2_parsed = parse_operand(operand2)
-
-        operation_table = {
-            '+': lambda: operand1_parsed + operand2_parsed,
-            '*': lambda: operand1_parsed * operand2_parsed,
-        }
-
-        item.worry_level = operation_table[operator]()
+        item.worry_level = eval(code, {'old': item.worry_level})
 
     return operation
+
+
+def value_divisible_by(value: int, divisor: int):
+    return value % divisor == 0
 
 
 class TurnResult(NamedTuple):
